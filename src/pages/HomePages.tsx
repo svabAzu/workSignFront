b8c18ee29471c72f892dab129d57eea9d044e2c9
RionakAsume <imlauerpablo@gmail.com>
2025-08-17 05:21:38 -0300

    Sidebar terminado iconos cargados y enrutamiento terminado.

---
+++ src/pages/HomePages.tsx
@@ -0,0 +1,8 @@
+export const HomePages = () => {
+    return (
+        <div className="flex flex-col items-start justify-center h-[95vh] bg-[#F1F1F1]">
+            <div className="h-[95%] w-[98%] bg-white rounded-4xl flex justify-center"> HomePage
+            </div>
+        </div>
+    )
+}
--- src/pages/RegisterPages.tsx
+++ src/pages/RegisterPages.tsx
@@ -1,5 +1,8 @@
 export const RegisterPages = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100"> Register </div>
+     <div className="flex flex-col items-start justify-center h-[95vh] bg-[#F1F1F1]">
+            <div className="h-[95%] w-[98%] bg-white rounded-4xl flex justify-center"> Register
+            </div>
+        </div>
   )
 }
