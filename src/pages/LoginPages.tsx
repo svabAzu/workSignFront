08550d40ad8dfbc16eef68e8a40b7454bf4129f9
RionakAsume <imlauerpablo@gmail.com>
2025-08-21 08:40:34 -0300

    Mejorar diseño y accesibilidad del formulario de inicio de sesión

--- src/pages/LoginPages.tsx
+++ src/pages/LoginPages.tsx
@@ -49,15 +49,20 @@ export const LoginPages = () => {
     <div className="flex flex-col items-start justify-center h-[95vh] bg-[#F1F1F1]">
       <div className="h-[95%] w-[98%] bg-white rounded-4xl flex justify-center">
 
-        <div className="h-screen w-full flex items-center justify-center">
-          <div className=" bg-green-600 w-1/4 flex items-center flex-col h-auto rounded-xl shadow-lg ">
+        <section className="w-1/2 flex flex-col items-center justify-center gap-4 ">
+           <img className="max-h-[50%] w-auto" src="/iconos/WorkSign-Engranaje.png" alt="Isotipo de la empresa" />
+           <h1 className="text-5xl font-bold">Bienvenido</h1>
+        </section>  
+
+        <div className="h-full w-1/2 flex items-center ">
+          <div className="bg-[#F1F1F1] w-[80%] flex items-center flex-col h-auto rounded-xl shadow-lg ">
             <div className="w-full flex justify-center items-center py-4 rounded-t-lg  bg-custom-green">
               {/* <LogoWhite /> */}
             </div>
-            <form onSubmit={handleSubmit} onReset={handleReset}>
-              <h2 className="text-white mt-3 font-bold mb-1">Usuario</h2>
+            <form onSubmit={handleSubmit} onReset={handleReset} className="w-[80%] flex flex-col mt-5 mb-5">
+              <h2 className="text-black mt-3 font-bold mb-1">Usuario</h2>
               <input
-                className="border border-gray-300 p-2 rounded w-52 flex justify-center"
+                className="border border-gray-400 p-2 rounded flex justify-center"
                 type="email"
                 placeholder="usuario@usuario.com"
                 name="email"
@@ -69,9 +74,9 @@ export const LoginPages = () => {
                 <span className="text-custom-yellow text-xs">Email invalido</span>
               )}
               <br />
-              <h2 className="text-[#ffff] mb-1 font-bold mt-1">Contraseña</h2>
+              <h2 className="text-black mb-1 font-bold mt-1">Contraseña</h2>
               <input
-                className="border border-[#f5f5f5] p-2 rounded w-52 flex justify-center"
+                className="border border-gray-400 p-2 rounded flex justify-center"
                 type="password"
                 placeholder="********"
                 name="password"
@@ -93,17 +98,19 @@ export const LoginPages = () => {
                 ))} */}
                 <div className="w-full mb-5 flex items-center justify-center">
                   <button
-                    className="mt-5 h-10 hover:bg-[#ffff] bg-custom-green text-[#ffff] font-bold hover:text-custom-green p-2 rounded"
+                    className=" flex justify-center items-center mt-5 p-6 w-40 h-10 hover:bg-[#ffff] bg-green-600 text-[#ffff] font-bold hover:text-custom-green rounded-2xl"
                     type="submit"
                   >
-                    Iniciar Sesión
+                    Ingresar
                   </button>
-                  <button
+
+
+                  {/* <button
                     className="mt-5 ml-3 h-10 hover:text-custom-green hover:bg-[#ffff] bg-custom-green text-[#ffff] p-2 rounded"
                     type="reset"
                   >
                     <PiBroomFill />
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </form>
