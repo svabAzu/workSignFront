efee88c0e206b191a0f1902aace3166fb4b22ad0
RionakAsume <imlauerpablo@gmail.com>
2025-08-24 22:12:49 -0300

    creación de la pagina settings y integracion con el registro y enrutado de los mismos

---
+++ src/pages/SettingsPages.tsx
@@ -0,0 +1,18 @@
+import { SidebarSettings } from "../components/settings/SidebarSettings";
+
+
+
+export const SettingsPages = () => {
+
+
+
+    return (
+        <main className="flex flex-col items-center h-auto bg-[#F1F1F1]">
+            <article className="h-[95%] w-full bg-white rounded-4xl flex flex-col p-6 shadow-lg">
+                {<SidebarSettings />}
+
+
+            </article>
+        </main>
+    )
+}
