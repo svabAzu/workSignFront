4627d0f6ceb443ad4e8f891e99564fe7aa91f670
RionakAsume <imlauerpablo@gmail.com>
2025-08-26 22:57:09 -0300

    mas cambios pequeños

--- src/pages/HomePages.tsx
+++ src/pages/HomePages.tsx
@@ -1,7 +1,7 @@
 export const HomePages = () => {
     return (
-        <div className="flex flex-col items-start justify-center h-[95vh] bg-[#F1F1F1]">
-            <div className="h-[95%] w-[98%] bg-white rounded-4xl flex justify-center"> HomePage
+        <div className="flex flex-col items-start w-[100%]  justify-center  bg-[#F1F1F1]">
+            <div className="w-[98%] h-auto my-4 rounded-4xl min-h-[98dvh] bg-white  items-center flex flex-col p-6 shadow-lg"> HomePage
             </div>
         </div>
     )
